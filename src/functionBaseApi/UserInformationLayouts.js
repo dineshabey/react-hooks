382c5a16b147b9ec236e7dd1f49e23d3cdf57c23
unknown <dinabeysinghe@gmail.com>
2023-09-27 13:11:04 +0530

    add userEffect

--- src/functionBaseApi/UserInformationLayouts.js
+++ src/functionBaseApi/UserInformationLayouts.js
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react'
 
 export default function UserInformationLayouts() {
 
-useEffect(()=>{})
+useEffect(()=>{},[]);
 
   return (
     <div>UserInformationLayouts</div>
